5ec13c5f943cdfc1c108ebbfa851f41b6eff90fd
Boat <kunlatidalawilai@gmail.com>
2018-06-21 18:13:03 +0700

    add history page

--- history.php
+++ history.php
@@ -19,34 +19,315 @@
 <!-- ------ start help_center ------------- -->
 
 <div class="container my-5 px-0">
-  <div class="bg-white login-page_pad login-page_boxshadow">
+  <div class="nc-thumbnail">
       <div class="row">
         <div class="col-12 col-md-6">
-          <div class="mb-4">
+          <div class="mt-2 text-md-left text-center">
             <h5 class="head-history">ประวัติการทำรายการ</h5>
           </div>
         </div>
         <div class="col-12 col-md-6">
           <form class="form-inline">
-  <div class="form-group input-group ml-auto">
-    <label for="inputPassword6">Search </label>
-    <input type="text" class="form-control ml-sm-3" id="password" placeholder="Search">
-    <button class="btn bg-blue my-2 my-sm-0" type="submit" style="border-radius: 0rem;"><i class="fas fa-search text-white"></i></button>
-  </div>
-</form>
-           <!-- <div class="input-group mb-lg-4  mb-3 mr-sm-2">
-              <input type="text" class="form-control" id="password" placeholder="S">
-              <div class="input-group-prepend">
-                  <div class="input-group-text bg-blue"><i class="fas fa-search text-white"></i></div>
-              </div>
-            </div> -->
+            <div class="form-group input-group ml-auto">
+              <label for="inputPassword6">Search </label>
+              <input type="text" class="form-control ml-3" id="password" placeholder="Search">
+              <button class="btn bg-blue my-sm-0" type="submit" style="border-radius: 0rem;"><i class="fas fa-search text-white"></i></button>
+            </div>
+          </form>
         </div>
       </div>
+
+    <!-- Start Item -->
       <div class="nc-card-item">
         <div class="nc-card-info">
-          <p>rgfghdfdfdfdfdfdfdfdfdfdfg</p>
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-5">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th>เลข PO :</th>
+                    <td>8051545</td>
+                  </tr>
+                  <tr>
+                    <th>ประเภทสินค้า :</th>
+                    <td>ม้วนผ้า</td>
+                  </tr>
+                  <tr>
+                    <th >จำนวน :</th>
+                    <td>20 ชิ้น</td>
+                  </tr>
+                  <tr>
+                    <th>จำนวนแพคเกจ :</th>
+                    <td>6 แพคเกจ</td>
+                  </tr>
+                </table>
+              </div>
+
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th>รหัสลูกค้า :</th>
+                    <td>nc-2224</td>
+                  </tr>
+                  <tr>
+                    <th>สถานะ :</th>
+                    <td>เสร็จสิ้น</td>
+                  </tr>
+                  <tr>
+                    <th >วันที่รับสินค้า :</th>
+                    <td>02/04/2561</td>
+                  </tr>
+                  <tr>
+                    <th>จัดส่งภายในวันที่ :</th>
+                    <td>30/04/2561</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="col-12 col-lg-3 mt-3 text-lg-right text-center">
+              <a href="history_detail.php" class="nc-btn">รายละเอียด</a>
+            </div>
+          </div>
         </div>
       </div>
+    <!-- End Item -->
+    <!-- Start Item -->
+      <div class="nc-card-item">
+        <div class="nc-card-info">
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-5">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th>เลข PO :</th>
+                    <td>8051545</td>
+                  </tr>
+                  <tr>
+                    <th>ประเภทสินค้า :</th>
+                    <td>ม้วนผ้า</td>
+                  </tr>
+                  <tr>
+                    <th >จำนวน :</th>
+                    <td>20 ชิ้น</td>
+                  </tr>
+                  <tr>
+                    <th>จำนวนแพคเกจ :</th>
+                    <td>6 แพคเกจ</td>
+                  </tr>
+                </table>
+              </div>
+
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th>รหัสลูกค้า :</th>
+                    <td>nc-2224</td>
+                  </tr>
+                  <tr>
+                    <th>สถานะ :</th>
+                    <td>เสร็จสิ้น</td>
+                  </tr>
+                  <tr>
+                    <th >วันที่รับสินค้า :</th>
+                    <td>02/04/2561</td>
+                  </tr>
+                  <tr>
+                    <th>จัดส่งภายในวันที่ :</th>
+                    <td>30/04/2561</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="col-12 col-lg-3 mt-3 text-lg-right text-center">
+              <a href="history_detail.php" class="nc-btn">รายละเอียด</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- End Item -->
+    <!-- Start Item -->
+      <div class="nc-card-item">
+        <div class="nc-card-info">
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-5">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th>เลข PO :</th>
+                    <td>8051545</td>
+                  </tr>
+                  <tr>
+                    <th>ประเภทสินค้า :</th>
+                    <td>ม้วนผ้า</td>
+                  </tr>
+                  <tr>
+                    <th >จำนวน :</th>
+                    <td>20 ชิ้น</td>
+                  </tr>
+                  <tr>
+                    <th>จำนวนแพคเกจ :</th>
+                    <td>6 แพคเกจ</td>
+                  </tr>
+                </table>
+              </div>
+
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th>รหัสลูกค้า :</th>
+                    <td>nc-2224</td>
+                  </tr>
+                  <tr>
+                    <th>สถานะ :</th>
+                    <td>เสร็จสิ้น</td>
+                  </tr>
+                  <tr>
+                    <th >วันที่รับสินค้า :</th>
+                    <td>02/04/2561</td>
+                  </tr>
+                  <tr>
+                    <th>จัดส่งภายในวันที่ :</th>
+                    <td>30/04/2561</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="col-12 col-lg-3 mt-3 text-lg-right text-center">
+              <a href="history_detail.php" class="nc-btn">รายละเอียด</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- End Item -->
+    <!-- Start Item -->
+      <div class="nc-card-item">
+        <div class="nc-card-info">
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-5">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th>เลข PO :</th>
+                    <td>8051545</td>
+                  </tr>
+                  <tr>
+                    <th>ประเภทสินค้า :</th>
+                    <td>ม้วนผ้า</td>
+                  </tr>
+                  <tr>
+                    <th >จำนวน :</th>
+                    <td>20 ชิ้น</td>
+                  </tr>
+                  <tr>
+                    <th>จำนวนแพคเกจ :</th>
+                    <td>6 แพคเกจ</td>
+                  </tr>
+                </table>
+              </div>
+
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th>รหัสลูกค้า :</th>
+                    <td>nc-2224</td>
+                  </tr>
+                  <tr>
+                    <th>สถานะ :</th>
+                    <td>เสร็จสิ้น</td>
+                  </tr>
+                  <tr>
+                    <th >วันที่รับสินค้า :</th>
+                    <td>02/04/2561</td>
+                  </tr>
+                  <tr>
+                    <th>จัดส่งภายในวันที่ :</th>
+                    <td>30/04/2561</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="col-12 col-lg-3 mt-3 text-lg-right text-center">
+              <a href="history_detail.php" class="nc-btn">รายละเอียด</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- End Item -->
+    <!-- Start Item -->
+      <div class="nc-card-item">
+        <div class="nc-card-info">
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-5">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th>เลข PO :</th>
+                    <td>8051545</td>
+                  </tr>
+                  <tr>
+                    <th>ประเภทสินค้า :</th>
+                    <td>ม้วนผ้า</td>
+                  </tr>
+                  <tr>
+                    <th >จำนวน :</th>
+                    <td>20 ชิ้น</td>
+                  </tr>
+                  <tr>
+                    <th>จำนวนแพคเกจ :</th>
+                    <td>6 แพคเกจ</td>
+                  </tr>
+                </table>
+              </div>
+
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th>รหัสลูกค้า :</th>
+                    <td>nc-2224</td>
+                  </tr>
+                  <tr>
+                    <th>สถานะ :</th>
+                    <td>เสร็จสิ้น</td>
+                  </tr>
+                  <tr>
+                    <th >วันที่รับสินค้า :</th>
+                    <td>02/04/2561</td>
+                  </tr>
+                  <tr>
+                    <th>จัดส่งภายในวันที่ :</th>
+                    <td>30/04/2561</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="col-12 col-lg-3 mt-3 text-lg-right text-center">
+              <a href="history_detail.php" class="nc-btn">รายละเอียด</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- End Item -->
+
+    <!-- pagination -->
+      <div class="container mt-5">                  
+        <ul class="pagination justify-content-center">
+          <li class="page-item pr-1"><a class="news_page-link" href="javascript:void(0);"><i class="fas fa-angle-left"></i></a></li>
+          <li class="page-item pr-1"><a class="news_page-link" href="javascript:void(0);">1</a></li>
+          <li class="page-item pr-1"><a class="news_page-link" href="javascript:void(0);">2</a></li>
+          <li class="page-item pr-1"><a class="news_page-link" href="javascript:void(0);">3</a></li>
+          <li class="page-item pr-1"><a class="news_page-link" href="javascript:void(0);"><i class="fas fa-angle-right"></i></a></li>
+        </ul>
+      </div> 
   </div>
 
 </div>
---
+++ history_detail.php
@@ -0,0 +1,258 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- START HEADER -->
+    <?php include_once('inc/header.php'); ?>
+    <!-- END HEADER -->
+  </head>
+
+  <body>
+  <!-- START NAVBAR -->
+    <?php include_once('inc/navbar.php'); ?>
+    <!-- END NAVBAR -->
+<div class="wrapper">
+
+    <!-- START CONTENT -->
+
+
+
+<!-- ------ start help_center ------------- -->
+
+<div class="container my-5 px-0">
+  <div class="nc-thumbnail">
+      <div class="row">
+        <div class="col-12 col-md-6">
+          <div class="mt-2 text-md-left text-center">
+            <h5 class="head-history">ประวัติการทำรายการ</h5>
+          </div>
+        </div>
+        <div class="col-12 col-md-6">
+          <form class="form-inline">
+            <div class="form-group input-group ml-auto">
+              <label for="inputPassword6">Search </label>
+              <input type="text" class="form-control ml-3" id="password" placeholder="Search">
+              <button class="btn bg-blue my-sm-0" type="submit" style="border-radius: 0rem;"><i class="fas fa-search text-white"></i></button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+    <!-- Start Item -->
+      <div class="nc-card-item">
+
+        <!-- Start Step -->
+        <div class="container">
+          <div class="process">
+            <div class="process-row">
+                <div class="process-step active">
+                  <div class="process-icon">
+                      <i class="flaticon-china fa-3x"></i>
+                      <p>รับสินค้าเข้าโกดังจีน</p>
+                  </div>
+                </div>
+                    <div class="process-step">
+                        <i class="fas fa-chevron-right"></i>
+                    </div>
+                <div class="process-step">
+                    <i class="flaticon-worldwide fa-3x"></i>
+                    <p>ปิดตู้ สินค้าเดินทางมาไทย</p>
+                </div>
+                    <div class="process-step">
+                        <i class="fas fa-chevron-right"></i>
+                    </div>
+                <div class="process-step">
+                    <i class="flaticon-map fa-3x"></i>
+                    <p>รับสินค้าเข้าโกดังไทย</p>
+                </div> 
+                    <div class="process-step">
+                        <i class="fas fa-chevron-right"></i>
+                    </div>
+                 <div class="process-step">
+                    <i class="flaticon-shipped fa-3x"></i>
+                    <p>จัดส่งสิ้นค้าภายในประเทศ</p>
+                </div> 
+                    <div class="process-step">
+                        <i class="fas fa-chevron-right"></i>
+                    </div>
+                <div class="process-step">
+                    <i class="flaticon-box fa-3x"></i>
+                    <p>เสร็จสิ้น</p>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- End Step -->
+
+        <div class="nc-card-info mb-4">
+          <div class="row">
+            <div class="col-12 col-md-6">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th width="160px">เลข PO :</th>
+                    <td>8051545</td>
+                  </tr>
+                  <tr>
+                    <th>ประเภทสินค้า :</th>
+                    <td>ม้วนผ้า</td>
+                  </tr>
+                  <tr>
+                    <th >จำนวน :</th>
+                    <td>20 ชิ้น</td>
+                  </tr>
+                  <tr>
+                    <th>จำนวนแพคเกจ :</th>
+                    <td>6 แพคเกจ</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="col-12 col-md-6">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th width="160px">รหัสลูกค้า :</th>
+                    <td>nc-2224</td>
+                  </tr>
+                  <tr>
+                    <th>สถานะ :</th>
+                    <td>เสร็จสิ้น</td>
+                  </tr>
+                  <tr>
+                    <th >วันที่รับสินค้า :</th>
+                    <td>02/04/2561</td>
+                  </tr>
+                  <tr>
+                    <th>จัดส่งภายในวันที่ :</th>
+                    <td>30/04/2561</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="nc-card-info mb-4">
+          <div class="row">
+            <div class="col-12 col-md-6">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th width="160px">รหัสลูกค้า :</th>
+                    <td>8051545</td>
+                  </tr>
+                  <tr>
+                    <th>เบอร์โทรศัพท์ :</th>
+                    <td>ม้วนผ้า</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="col-12 col-md-6">
+              <div class="table-responsive">
+                <table class="tb-detail">
+                  <tr>
+                    <th width="160px">ที่อยู่จัดส่ง :</th>
+                    <td>8/8หมู่ 4 ซอย ถนนบางกรวย-ไทยน้อย ตำบลโสนลอย 
+                    อำเภอ บางบัวทอง จังหวัดนนทบุรี 11110</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="alert alert-warning" role="alert">
+          <span>*หมายเหตุ : </span>
+        </div>
+
+      </div>
+    <!-- End Item -->
+
+    <!-- Start Item -->
+      <div class="nc-card-item">
+        <div class="table-responsive">
+          <table class="table table-bordered text-center">
+            <thead>
+              <tr>
+                <th scope="col" class="no-wrap">Description of Good</th>
+                <th scope="col" class="no-wrap">Package</th>
+                <th scope="col" class="no-wrap">จำนวนสินค้าในกล่องตามที่ร้านค้าจีนแจ้ง</th>
+                <th scope="col" class="no-wrap">KG</th>
+                <th scope="col" class="no-wrap">Cubic Meter</th>
+                <th scope="col" class="no-wrap">Remark</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="text-left">Knitted clothe-BOB</td>
+                <td>6</td>
+                <td>10</td>
+                <td>133.20</td>
+                <td>0.759</td>
+                <td>EK</td>
+              </tr>
+              <tr>
+                <td class="text-left">Knitted clothe-BOB</td>
+                <td>6</td>
+                <td>10</td>
+                <td>133.20</td>
+                <td>0.759</td>
+                <td>EK</td>
+              </tr>
+              <tr>
+                <td class="text-left">Knitted clothe-BOB</td>
+                <td>6</td>
+                <td>10</td>
+                <td>133.20</td>
+                <td>0.759</td>
+                <td>EK</td>
+              </tr>
+              <tr>
+                <td class="text-left">Knitted clothe-BOB</td>
+                <td>6</td>
+                <td>10</td>
+                <td>133.20</td>
+                <td>0.759</td>
+                <td>EK</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        
+      </div>
+    <!-- End Item -->  
+
+    <div class="row">
+      <div class="col-12 col-md-10 text-md-left text-center">
+        <b>หากมีปัญหาหรือข้อสงสัย กรุณาติดต่อ (คุณสมร) : 088-222-3333 หรือ LinrID : @New_cargo</b>
+      </div>
+      <div class="col-12 col-md-2 mt-4 mt-lg-0 text-lg-right text-center">
+        <a href="history.php" class="nc-btn">กลับ</a>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+
+
+<!-- ------ end help_center ------------- -->
+
+
+    <!-- END CONTENT -->
+</div>
+
+    <!-- START FOOTER -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- END FOOTER -->
+
+    <!-- START FOOTER SCRIPT -->
+    <?php include_once('inc/footer-script.php'); ?>
+    <!-- END FOOOTER SCRIPT -->
+
+
+  </body>
+</html>
